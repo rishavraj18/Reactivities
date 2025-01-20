d8797f2a589d549a3a0822c89c2a5284dc0fae48
rishavraj <fullstackdev.rishav@gmail.com>
2025-01-20 19:57:21 +0530

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(option => 
+{
+    option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    // app.UseSwaggerUI(options => 
+    // {
+    //     options.SwaggerEndpoint("/openapi/v1.json", "Demo API");
+    // });
+}
+
+app.MapControllers();
+
+// EF migration code
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "An error occured during migration");
+}
+// upto here EF migration code
+
+app.Run();
