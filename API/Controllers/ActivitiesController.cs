bff8599d1e378f097d03de922c7896d0cfcf415f
rishavraj <fullstackdev.rishav@gmail.com>
2025-03-01 07:33:19 +0530

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,8 @@
 
+using Application.Activities.Commands;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,7 +10,7 @@ using Persistence;
 namespace API.Controllers
 {   
     // New method i.e. Primary constructor
-    public class ActivitiesController(AppDbContext context) : BaseApiController
+    public class ActivitiesController(IMediator mediator) : BaseApiController
     {   
         // Old method
         
@@ -21,17 +24,35 @@ namespace API.Controllers
         [HttpGet] // api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await context.Activities.ToListAsync();
+            return await mediator.Send(new GetActivityList.Query());
         }
 
         [HttpGet("{id}")] // api/activities/guid
         public async Task<ActionResult<Activity>> GetActivity(string id)
         {
-            var activity = await context.Activities.FindAsync(id);
+            return await mediator.Send(new GetActivityDetails.Query{Id = id});
+        }
 
-            if(activity == null) return NotFound(); // 404
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command{Activity = activity});
+        }
 
-            return activity;
+        [HttpPut]
+        public async Task<ActionResult> EditActivity(Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command{Activity = activity});
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(string id)
+        {
+            await Mediator.Send(new DeleteActivity.Command{Id = id});
+
+            return Ok();
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Scalar.AspNetCore;
@@ -14,6 +16,9 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => 
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
